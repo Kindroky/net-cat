64c8edfa14ef2a380e1d9bd64b1f089584545208
alebourg <abelux662@gmail.com>
2024-09-25 14:45:18 +0200

    Linux teleported in VSCODE

--- server.go
+++ server.go
@@ -57,7 +57,24 @@ func main() {
 }
 
 func HandleUsername(conn net.Conn) string {
-	conn.Write([]byte("Please enter username : "))
+	conn.Write([]byte(`Welcome to TCP-Chat!
+         _nnnn_
+        dGGGGMMb
+       @p~qp~~qMb
+       M|@||@) M|
+       @,----.JM|
+      JS^\__/  qKL
+     dZP        qKRb
+    dZP          qKKb
+   fZP            SMMb
+   HZM            MMMM
+   FqM            MMMM
+ __| ".        |\dS"qML
+ |    ` + "`" + `.       | ` + "`" + `' \Zq
+_)      \.___.,|     .'
+\____   )MMMMMP|   .'
+     ` + "`" + `-'       ` + "`" + `--'
+[ENTER YOUR NAME]:`))
 	buf := bufio.NewScanner(conn)
 	for {
 		buf.Scan()
